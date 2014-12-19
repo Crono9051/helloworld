020c697dba7a6fe8b342b227fb930aafae7d0435
Crono9051 <crono9051@gmail.com>
2014-12-18 16:34:18 -0800

    Maven Test Project

---
+++ helloworld/src/main/java/com/programcreek/helloworld/Hello.java
@@ -0,0 +1,27 @@
+package com.programcreek.helloworld;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.programcreek.helloworld.services.HelloWorldService;
+
+public class Hello {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		HelloWorldService service = (HelloWorldService) context.getBean("helloWorldService");
+		
+		String message = service.sayHello();
+		System.out.println(message);
+		
+		//set a new name
+		service.setName("Spring");
+		message = service.sayHello();
+		System.out.println(message);
+				
+	}
+
+}
